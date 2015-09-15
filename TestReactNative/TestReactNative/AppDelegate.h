7c15c4005cec5e34c8b0711fb03c227f7f08e8c7
ren <ren@rens-Mac-mini.local>
2015-09-15 17:46:18 +0800

    1.首次提交，测试reactnative（Facebook）

---
+++ TestReactNative/TestReactNative/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestReactNative
+//
+//  Created by ren on 9/15/15.
+//  Copyright (c) 2015 ren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
